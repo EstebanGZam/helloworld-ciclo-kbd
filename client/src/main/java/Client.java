503e8a5a7ddf307de028113fb09c2ee371b046a8
EstebanGZam <estebangaviria82@gmail.com>
2024-09-08 16:08:24 -0500

    feat: clients messages

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,21 +1,44 @@
+import Demo.PrinterPrx;
 import Demo.Response;
+import com.zeroc.Ice.Communicator;
+import com.zeroc.Ice.Util;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Client {
-	public static void main(String[] args) {
-		java.util.List<String> extraArgs = new java.util.ArrayList<>();
+	public static Scanner scanner = new Scanner(System.in);
 
-		try (com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args, "client.cfg", extraArgs)) {
+	public static void main(String[] args) {
+		List<String> extraArgs = new ArrayList<>();
+
+		try (Communicator communicator = Util.initialize(args, "client.cfg", extraArgs)) {
 			//com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -p 10000");
 			Response response;
-			Demo.PrinterPrx service = Demo.PrinterPrx
+			PrinterPrx service = PrinterPrx
 					.checkedCast(communicator.propertyToProxy("Printer.Proxy"));
 
 			if (service == null) {
 				throw new Error("Invalid proxy");
 			}
-			response = service.printString("Hello World from a remote client!");
+			String username = System.getProperty("user.name");
+			String hostname = Inet4Address.getLocalHost().getHostName();
+			boolean exit = false;
+			String input;
 
-			System.out.println("Respuesta del server: " + response.value + ", " + response.responseTime);
+			while (!exit) {
+				String prefix = username + ":" + hostname + " ";
+				System.out.print(prefix);
+				input = scanner.nextLine();
+				response = service.printString(prefix + input);
+				exit = input.equalsIgnoreCase("exit");
+				System.out.println("Server response: " + response.value);
+			}
+		} catch (UnknownHostException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
